207fafa68171fc9d90531a1a53423276eba0a327
SaidRem <said.r@yandex.ru>
2021-03-11 19:20:41 +0400

    Added mode all

--- console_quiz_capitals.py
+++ console_quiz_capitals.py
@@ -5,16 +5,21 @@ COUNTRIES = [europe['Europe'], asia['Asia'], africa['Africa'], america['America'
 
 def cap_quiz_rus():
     print('Тест по столицам стран.\nВыберите часть света: ')
-    print('Europe(0), Asia(1), Africa(2), America(3), Australia(4)')
+    print('Europe(0), Asia(1), Africa(2), America(3), Australia(4), All(5)')
     while True:
         try:
             num = int(input('Введите цифру: '))
-            if num > 4 or num < 0:
+            if num > 5 or num < 0:
                 continue
             break
         except ValueError:
             pass
-    country_caps = COUNTRIES[num]
+    if num == 5:
+        country_caps = {}
+        for c in COUNTRIES:
+            country_caps.update(c)
+    else:
+        country_caps = COUNTRIES[num]
     countries_list = list(country_caps.keys())
     random.shuffle(countries_list)
     countries_amount = len(countries_list)
@@ -23,9 +28,9 @@ def cap_quiz_rus():
         n = random.randint(0, countries_amount)
         player_choice = input(f'Столица гос-ва {countries_list[n]}\n=> ')
         if player_choice == country_caps[countries_list[n]]:
-            print('Совершенно верно!')
+            print('Совершенно верно!\n')
         else:
-            print(f'Неверно.\nПравильный ответ: {country_caps[countries_list[n]]}')
+            print(f'Неверно.\nПравильный ответ: {country_caps[countries_list[n]]}\n')
     input('Game over')
 
 
